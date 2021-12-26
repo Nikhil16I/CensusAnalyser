c3492fb5f2a62db7d4592a50e1da5a384b68d2c4
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-12-26 23:08:33 +0530

    Added Method to Load Data from CSV file & T.C 1.1 Passed

---
+++ src/com/Test/CensusAnalyserTest.java
@@ -0,0 +1,19 @@
+package com.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.censu_analyser.analyser.StateCensusAnalyser;
+public class CensusAnalyserTest {
+
+	/*Testing the CSV file to be Read*/
+	@Test
+	public void testcensus()throws Exception{
+		StateCensusAnalyser analyser = new StateCensusAnalyser();
+		analyser.loadCensusData();
+		assertEquals(true, analyser.censusEntries());
+
+	}
+
+}
